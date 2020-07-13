09592d61826a941244a18cd0b37d9c72468b4273
pelousous <davidepelo@yahoo.it>
2020-07-13 13:30:52 +0000

    added redux-thunk to manage async call

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,20 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOT_PENDING,
+    REQUEST_ROBOT_SUCCESS,
+    REQUEST_ROBOT_FAILED 
+} from './constants.js';
 
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text    
 })
+/* when it returns a function and not a state it pass through 
+   the thunk middleware that manage asyncs calls */
+export const requestRobots = () => (dispatch) => {
+    dispatch({type: REQUEST_ROBOT_PENDING});
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(data => dispatch({type: REQUEST_ROBOT_SUCCESS, payload: data}))
+      .catch(error => dispatch({type: REQUEST_ROBOT_FAILED, payload: error}));
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+/* use to manage aynchronous actions in redux */
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers.js';
+import { searchRobots, requestRobots } from './reducers.js';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+
+/* combineReducer is a function used to compose reducers in a root reducer */
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+/* the middlewares are applied in order */
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(<Provider store={store}>
                     <App />
